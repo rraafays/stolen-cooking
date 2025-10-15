fff7e1a7ec3ca4eadf4e79aa69ace6832a453dfa
raf <rraf@tuta.io>
2025-10-15 05:08:09 +0100

    chore: wrote flake

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs =
+            with pkgs;
+            [
+              bun
+            ];
+        };
+      }
+    );
+}
